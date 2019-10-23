a28dea60567f2ec94a9d5602615763e15fb24907
Logon Aluno <logonaluno@local.com>
2019-10-23 10:34:13 -0300

    fechando conexoes

--- src/main/java/br/com/fiap/levelAI/conexao/Conexao.java
+++ src/main/java/br/com/fiap/levelAI/conexao/Conexao.java
@@ -2,11 +2,17 @@ package br.com.fiap.levelAI.conexao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexao {
-
+	private static Connection con;
+	
 	public static Connection getConectar() throws Exception {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		return DriverManager.getConnection("jdbc:oracle:thin:@banco.fiap.com.br:1521:ORCL", "RM81830", "070791");
+	}
+	
+	public static void fechar() throws SQLException {
+		con.close();
 	}
 }
